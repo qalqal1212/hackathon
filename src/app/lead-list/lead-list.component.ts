a8a17787053c9a39d14360e328e6710b09197ae4
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2024-09-25 12:17:12 +0800

    Update Lead List page CSS

--- src/app/lead-list/lead-list.component.ts
+++ src/app/lead-list/lead-list.component.ts
@@ -4,36 +4,35 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-lead-list',
   templateUrl: './lead-list.component.html',
-  styleUrls: ['./lead-list.component.scss']
+  styleUrls: ['./lead-list.component.scss'],
 })
 export class LeadListComponent {
-
-  constructor(private router: Router){}
+  constructor(private router: Router) {}
 
   dataList = [
     {
-      name: "Hello",
-      id: "12345",
-      contact: "012-5141477",
-      date: "12/08/2024",
-      status: "Active",
-      brn: "1408874K"
+      name: 'Alex Yap Sdn. Bhd.',
+      id: '1408874K',
+      contactName: 'Yap Yaw Wah',
+      contact: '012-5141477',
+      date: '12/08/2024',
+      status: 'Active',
+      brn: '1408874K',
     },
     {
-      name: "World",
-      id: "67890",
-      contact: "014-5482147",
-      date: "15/09/2024",
-      status: "Pending",
-      brn: "1408888K"
-    }
+      name: 'Hackstreet Corp.',
+      id: '1408888K',
+      contactName: 'Kevin Richardson',
+      contact: '014-5482147',
+      date: '15/09/2024',
+      status: 'Pending',
+      brn: '1408888K',
+    },
   ];
 
-
-  upArrowAccordion = "assets/images/icons/up-arrow-cherry.svg";
-  downArrowAccordion = "assets/images/icons/down-arrow-cherry.svg";
-  arrowImg = "assets/images/icons/right-arrow.svg";
-
+  upArrowAccordion = 'assets/images/icons/up-arrow-cherry.svg';
+  downArrowAccordion = 'assets/images/icons/down-arrow-cherry.svg';
+  arrowImg = 'assets/images/icons/right-arrow.svg';
 
   onNextButton(brn: string) {
     // if (this.myForm.valid) {
